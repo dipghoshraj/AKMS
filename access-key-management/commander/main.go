8642d7b194c9b566fb2486b3a8cbce53748b3ad9
dipghoshraj <dipghoshraj@gmail.com>
2025-05-18 21:44:02 +0530

    feat : starting of the projects

---
+++ access-key-management/commander/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+	// This is a simple Go program that prints "Hello, World!" to the console.
+	// It serves as a starting point for building a command-line application.
+	// You can add more functionality and features as needed.
+	// For example, you can use the "flag" package to parse command-line arguments,
+	// or use the "os" package to interact with the file system.
+	// Happy coding!
+}
