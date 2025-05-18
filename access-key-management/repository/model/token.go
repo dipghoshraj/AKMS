5426722c1c1267b6ef9b635af1bc885c40795a53
dipghoshraj <dipghoshraj@gmail.com>
2025-05-18 23:43:44 +0530

    feat : model for token

---
+++ access-key-management/repository/model/token.go
@@ -0,0 +1,13 @@
+package model
+
+import "time"
+
+type Token struct {
+	ID                 int64     `gorm:"primaryKey;autoIncrement" json:"id"`
+	Hashkey            string    `gorm:"size:255;not null" json:"hashkey"`
+	RateLimitPerMinute int64     `gorm:"not null" json:"rate_limit_per_minute"`
+	ExpiresAt          time.Time `gorm:"not null" json:"expires_at"`
+	Disabled           bool      `gorm:"not null" json:"disabled"`
+	CreatedAt          time.Time `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt          time.Time `gorm:"autoUpdateTime" json:"updated_at"`
+}
