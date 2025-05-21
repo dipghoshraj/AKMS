705f807a4f111b271ee8e326f3e264200ad88bab
dipghoshraj <dipghoshraj@gmail.com>
2025-05-21 16:40:08 +0530

    feat : add redis push

--- token-verify-service/dbops/tokenOps.go
+++ token-verify-service/dbops/tokenOps.go
@@ -2,8 +2,10 @@ package dbops
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
+	"time"
 	"tvs/dbops/model"
 )
 
@@ -17,6 +19,12 @@ func (t *tokenOps) Save(ctx context.Context, reqID string, token *model.Token) e
 		log.Printf("[request_id=%s] Failed to update token: %v", reqID, err)
 		return err
 	}
+
+	go func() {
+		if err := t.SetRedis(ctx, token.Hashkey, reqID); err != nil {
+			log.Printf("[request_id=%s] Failed to set token in Redis: %v", reqID, err)
+		}
+	}()
 	return nil
 }
 
@@ -32,6 +40,41 @@ func (t *tokenOps) DisableToken(ctx context.Context, disableMesage model.Disable
 		log.Printf("[request_id=%s] Failed to update token: %v", disableMesage.ReqID, "token not found")
 		return fmt.Errorf("token not found")
 	}
+
+	go func() {
+		if err := t.SetRedis(ctx, disableMesage.HashKey, disableMesage.ReqID); err != nil {
+			log.Printf("[request_id=%s] Failed to set token in Redis: %v", disableMesage.ReqID, err)
+		}
+	}()
+
+	return nil
+}
+
+func (t *tokenOps) SetRedis(ctx context.Context, key string, reqID string) error {
+	var token model.Token
+	if err := t.db.WithContext(ctx).Where("hashkey = ?", key).First(&token).Error; err != nil {
+		log.Printf("[request_id=%s] Failed to find token: %v", key, err)
+		return err
+	}
+
+	redisValue := map[string]any{
+		"disabled":   token.Disabled,
+		"rate_limit": token.RateLimitPerMinute,
+		"expired_at": token.ExpiresAt}
+
+	value, err := json.Marshal(redisValue)
+	if err != nil {
+		log.Printf("[request_id=%s] Failed to marshal token: %v", reqID, err)
+		return err
+	}
+
+	ttl := 5 * time.Minute
+	if err := t.redis.Set(ctx, key, value, ttl).Err(); err != nil {
+		log.Printf("[request_id=%s] Failed to set token in Redis: %v", key, err)
+		return err
+	}
+
+	log.Printf("[request_id=%s] Token set in Redis: %s", reqID, key)
 
 	return nil
 }
