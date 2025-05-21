e6ab38d17083aa25ee2d797ef69d12409246818d
dipghoshraj <dipghoshraj@gmail.com>
2025-05-21 16:26:47 +0530

    feat : using redis

---
+++ token-verify-service/store/initRedis.go
@@ -0,0 +1,35 @@
+package store
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+var RedisClient *redis.Client
+
+func InitRedis() error {
+	client := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%s", GetEnv("REDIS_HOST", "localhost"), GetEnv("REDIS_PORT", "6379")),
+		Password: GetEnv("REDIS_PASSWORD", ""),
+		DB:       0,
+	})
+
+	// Test connection
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	_, err := client.Ping(ctx).Result()
+	if err != nil {
+		log.Fatalf("Failed to connect to Redis: %v", err)
+		return err
+	}
+
+	RedisClient = client
+	fmt.Println("Redis connection established successfully.")
+
+	return nil
+}
